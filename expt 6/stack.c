29d6d48064f1ea500e8f34ab64b9e427a933a8e7
enter-opy <vivekvijayan6282@gmail.com>
2022-03-08 17:11:14 +0530

    Update stack.c

--- expt 6/stack.c
+++ expt 6/stack.c
@@ -1,112 +1,53 @@
 #include <stdio.h>
-
 #include <stdlib.h>
+#include <conio.h>
 
-#include<conio.h>
+int s[5], top=-1;
 
-int s[5],top=-1;
-
-void push()
-
-{
-
-    if(top==4)
-
-        printf("\nStack overflow!!!!");
-
-    else
-
-    {
-
+void push(void) {
+    if (top == 4) printf("\nStack overflow!!!!");
+    else {
         printf("\nEnter element to insert:");
-
         scanf("%d",&s[++top]);
-
     }
-
 }
 
-void pop()
-
-{
-
-    if(top==-1)
-	printf("\nStack underflow!!!");
-
-    else
-
-        printf("\nElement popped is: %d",s[top--]);
-
+void pop(void) {
+    if (top == -1) printf("\nStack underflow!!!");
+    else printf("\nElement popped is: %d",s[top--]);
 }
 
-void disp()
+void disp(void) {
+    int t = top;
 
-{
+    if ( t == -1) printf("\nStack empty!!");
+    else printf("\nStack elements are:\n");
 
-    int t=top;
-
-    if(t==-1)
-
-        printf("\nStack empty!!");
-
-    else
-
-        printf("\nStack elements are:\n");
-
-    while(t>=0)
-
-        printf("%d ",s[t--]);
-
+    while (t >= 0) printf("%d ",s[t--]);
 }
 
 
-int main()
-
-{
-
+int main(void) {
     int ch;
 
-    do
-
-    {
-
+    do {
         printf("\n...Stack operations.....\n");
-
         printf("1.PUSH\n");
-
         printf("2.POP\n");
-
-
         printf("3.Display\n");
-
         printf("4.Exit\n________________\n");
-
         printf("Enter choice:");
 
         scanf("%d",&ch);
 
-        switch(ch)
-
-        {
-
-            case 1:push();break;
-
-            case 2:pop();break;
-
-           
-			
-			case 3:disp();break;
-
-            case 4:exit(0);
-
-            default:printf("\nInvalid choice");
-
+        switch(ch) {
+            case 1: push(); break;
+            case 2: pop(); break;
+            case 3: disp();break;
+            case 4: exit(0);
+            default: printf("\nInvalid choice");
         }
-
-    }
-
-    while(1);
+    } while(1);
 
     return 0;
-
 }
