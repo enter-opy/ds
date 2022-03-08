12ba2b26fd7ac129df847a561ee1400cb93beb5c
enter-opy <vivekvijayan6282@gmail.com>
2022-03-08 17:27:43 +0530

    Update queuell.c

--- expt9/queuell.c
+++ expt9/queuell.c
@@ -1,203 +1,103 @@
 #include<stdio.h>
-
 #include<stdlib.h>
 
-struct node
-
-{
-
+struct node {
     int data;
-
     struct node *next;
-
 };
 
 struct node *front;
 
 struct node *rear;
 
-void ins();
+void ins(void);
 
-void del();
+void del(void);
 
-void display();
+void display(void);
 
-void main ()
-
-{
-
+void main(void) {
     int choice;
 
-    while(choice != 4)
+    while (choice != 4) {
+        printf("\n1.Insert an element\n2.Delete an element\n3.Display the queue\n4.Exit\n");
+        printf("Enter your choice:");
+        scanf("%d",& choice);
 
-    {
+        switch(choice) {
+            case 1: ins();
+                break;
 
-printf("\n1.Insert an element\n2.Delete an element\n3.Display the queue\n4.Exit\n");
+            case 2: del();
+                break;
 
-printf("Enter your choice:");
+            case 3: display();
+                break;
 
-scanf("%d",& choice);
+            case 4: exit(0);
+                break;
 
-switch(choice)
-
-{
-
-    case 1:
-
-    ins();
-
-    break;
-
-    case 2:
-
-    del();
-
-    break;
-
-    case 3:
-
-    display();
-
-    break;
-
-    case 4:
-
-    exit(0);
-
-    break;
-
-    default:
-
-    printf("\nEnter valid choice!!");
-
-}
-
+            default: printf("\nEnter valid choice!!");
+        }
     }
-
 }
 
-void ins()
-
-{
-
+void ins(void) {
     struct node *ptr;
 
     int item;
 
-    ptr = (struct node *) malloc (sizeof(struct node));
+    ptr = (struct node *) malloc(sizeof(struct node));
 
-    if(ptr == NULL)
+    if (ptr == NULL) {
+        printf("\nOVERFLOW\n");
+        return;
+    } else {
+        printf("Enter value:");
+        scanf("%d", &item);
 
-    {
+        ptr -> data = item;
 
-printf("\nOVERFLOW\n");
-
-return;
-
+        if (front == NULL) {
+            front = ptr;
+            rear = ptr;
+            front -> next = NULL;
+            rear -> next = NULL;
+        } else {
+            rear -> next = ptr;
+            rear = ptr;
+            rear->next = NULL;
+        }
     }
-
-    else
-
-    {
-
-printf("Enter value:");
-
-scanf("%d",&item);
-
-ptr -> data = item;
-
-if(front == NULL)
-
-{
-
-    front = ptr;
-
-    rear = ptr;
-
-    front -> next = NULL;
-
-    rear -> next = NULL;
-
 }
 
-else
-
-{
-
-    rear -> next = ptr;
-
-    rear = ptr;
-
-    rear->next = NULL;
-
-}
-
-    }
-
-}
-
-void del ()
-
-{
-
+void del(void) {
     struct node *ptr;
 
-    if(front == NULL)
-
-    {
-
-printf("\nUNDERFLOW\n");
-
-return;
-
+    if (front == NULL) {
+        printf("\nUNDERFLOW\n");
+        return;
+    } else {
+        ptr = front;
+        front = front -> next;
+        free(ptr);
     }
-
-    else
-
-    {
-
-ptr = front;
-
-front = front -> next;
-
-free(ptr);
-
-    }
-
 }
 
-void display()
-
-{
-
+void display(void) {
     struct node *ptr;
-
     ptr = front;
 
-    if(front == NULL)
+    if (front == NULL) {
+        printf("\nEmpty queue");
+    } else {   
+        printf("Elements of Queue:");
 
-    {
+        while(ptr != NULL) {
+            printf(" %d",ptr -> data);
+            ptr = ptr -> next;
+        }
 
-printf("\nEmpty queue");
-
+        printf("\n");
     }
-
-    else
-
-    {   printf("Elements of Queue:");
-
-while(ptr != NULL)
-
-{
-
-    printf(" %d",ptr -> data);
-
-    ptr = ptr -> next;
-
-}
-
-   printf("\n");
-
-    }
-
 }
